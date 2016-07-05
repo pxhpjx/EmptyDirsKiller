81b5ee98e0b075e31f6bb9e52cfd99f28d8c0a9a
pxhpjx <pxhpjx@163.com>
2016-07-05 14:31:17 +0800

    删除所有空文件夹

---
+++ PP's EmptyDirsKiller/DirsKiller.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PP_s_EmptyDirsKiller
+{
+    partial class DirsKiller
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtMon = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(12, 12);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(260, 21);
+            this.txtPath.TabIndex = 0;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(294, 12);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "清理";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtMon
+            // 
+            this.txtMon.Location = new System.Drawing.Point(13, 51);
+            this.txtMon.Multiline = true;
+            this.txtMon.Name = "txtMon";
+            this.txtMon.ReadOnly = true;
+            this.txtMon.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMon.Size = new System.Drawing.Size(368, 199);
+            this.txtMon.TabIndex = 2;
+            // 
+            // DirsKiller
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 262);
+            this.Controls.Add(this.txtMon);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtPath);
+            this.Name = "DirsKiller";
+            this.Text = "PP\'s EmptyDirsKiller";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtMon;
+    }
+}
+
---
+++ PP's EmptyDirsKiller/DirsKiller.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PP_s_EmptyDirsKiller
+{
+    public partial class DirsKiller : Form
+    {
+        public DirsKiller()
+        {
+            InitializeComponent();
+        }
+
+        void AddMonText(string msg, bool isNeeedClearText = false)
+        {
+            if (isNeeedClearText)
+                txtMon.Text = null;
+            txtMon.AppendText(string.Format("{0} {1}\r\n", DateTime.Now.ToString("HH:mm:ss"), msg));
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            string path = txtPath.Text;
+            if (string.IsNullOrWhiteSpace(path) || !Directory.Exists(path))
+            {
+                AddMonText("不存在玩个杰宝！");
+                return;
+            }
+            CheckAndDestory(path);
+        }
+
+
+        void CheckAndDestory(string path)
+        {
+            if (!Directory.Exists(path))
+                return;
+            string[] dirs = Directory.GetDirectories(path, "*", SearchOption.TopDirectoryOnly);
+            if (dirs != null && dirs.Length > 0)
+            {
+                foreach (string dir in dirs)
+                    CheckAndDestory(dir);
+            }
+            dirs = Directory.GetDirectories(path, "*", SearchOption.TopDirectoryOnly);
+            string[] files = Directory.GetFiles(path, "*", SearchOption.TopDirectoryOnly);
+            if ((dirs == null || dirs.Length == 0) && (files == null || files.Length == 0))
+            {
+                Directory.Delete(path);
+                AddMonText("已清除：" + path);
+            }
+        }
+    }
+}
